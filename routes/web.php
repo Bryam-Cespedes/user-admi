262e95b4d48b605235b32c7f8436f95176babef3
Bryam Cespedes <bryam.cv2020@gmail.com>
2026-05-28 10:39:07 -0400

    Ruta principal login

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
+Route::get('/', function () {
+    return redirect('/login');
+});
+
 Route::middleware(['auth', 'admin'])->group(function () {
 
     Route::resource('usuarios', UserController::class);
